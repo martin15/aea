e96a8214ce131dddcd0e61e6066d1f135f685194
Martin <martin.me15@yahoo.com>
2016-04-07 01:30:41 +0700

    fix seed file

--- aea/db/seeds.rb
+++ aea/db/seeds.rb
@@ -8,30 +8,33 @@
 
 
 
-country_1 = Country.find_or_create_by_name( :name => 'Singapore', :country_type => "Developed" )
-country_2 = Country.find_or_create_by_name( :name => 'Malaysia', :country_type => "Developed" )
-country_3 = Country.find_or_create_by_name( :name => 'Japan', :country_type => "Developed" )
-country_4 = Country.find_or_create_by_name( :name => 'Korea', :country_type => "Developed" )
-country_5 = Country.find_or_create_by_name( :name => 'Hong Kong', :country_type => "Developed" )
-country_6 = Country.find_or_create_by_name( :name => 'Taiwan', :country_type => "Developed" )
-country_7 = Country.find_or_create_by_name( :name => 'Australia', :country_type => "Developed" )
-country_8 = Country.find_or_create_by_name( :name => 'New Zealand', :country_type => "Developed" )
-country_9 = Country.find_or_create_by_name( :name => 'USA', :country_type => "Developed" )
-country_10 = Country.find_or_create_by_name( :name => 'EU', :country_type => "Developed" )
-country_10 = Country.find_or_create_by_name( :name => 'Indonesia', :country_type => "Developing" )
+country_1 = Country.find_or_create_by( :name => 'Singapore', :category_type => "Developed" )
+country_2 = Country.find_or_create_by( :name => 'Malaysia', :category_type => "Developed" )
+country_3 = Country.find_or_create_by( :name => 'Japan', :category_type => "Developed" )
+country_4 = Country.find_or_create_by( :name => 'Korea', :category_type => "Developed" )
+country_5 = Country.find_or_create_by( :name => 'Hong Kong', :category_type => "Developed" )
+country_6 = Country.find_or_create_by( :name => 'Taiwan', :category_type => "Developed" )
+country_7 = Country.find_or_create_by( :name => 'Australia', :category_type => "Developed" )
+country_8 = Country.find_or_create_by( :name => 'New Zealand', :category_type => "Developed" )
+country_9 = Country.find_or_create_by( :name => 'USA', :category_type => "Developed" )
+country_10 = Country.find_or_create_by( :name => 'EU', :category_type => "Developed" )
+country_10 = Country.find_or_create_by( :name => 'Indonesia', :category_type => "Developing" )
 
 
-user_type_1 = UserType.find_or_create_by_name( :name => 'National Alliance' )
-user_type_2 = UserType.find_or_create_by_name( :name => 'Leader and Member of AEA Commissions' )
-user_type_3 = UserType.find_or_create_by_name( :name => 'AEA Executive Team Members' )
-user_type_4 = UserType.find_or_create_by_name( :name => 'Partner & Observers' )
-user_type_5 = UserType.find_or_create_by_name( :name => 'Representative of Local Leaders' )
+user_type_1 = UserType.find_or_create_by( :name => 'National Alliance' )
+user_type_2 = UserType.find_or_create_by( :name => 'Leader and Member of AEA Commissions' )
+user_type_3 = UserType.find_or_create_by( :name => 'AEA Executive Team Members' )
+user_type_4 = UserType.find_or_create_by( :name => 'Partner & Observers' )
+user_type_5 = UserType.find_or_create_by( :name => 'Representative of Local Leaders' )
 
-user_type_5 = User.find_or_create_by_email( :email => 'aeaga2016@gmail.com', :password => "30012016BDG",
-                                            :password_confirmation => "30012016BDG",
-                                            :first_name => "admin", :last_name => "admin",
-                                            :passport_number => "12345678", :age => "99",
-                                            :confirmed_at => Time.now, :country_id => country_10.id,
-                                            :user_type_id => user_type_5.id, :gender => 'male',
-                                            :title => "Mr")
+user  = User.find_by_email("aeaga2016@gmail.com")
+unless user.nil?
+  user_type_5 = User.create( :email => 'aeaga2016@gmail.com', :password => "30012016BDG",
+                             :password_confirmation => "30012016BDG",
+                             :first => "admin", :last => "admin",
+                             :passport_number => "12345678", :age => "99",
+                             :confirmed_at => Time.now, :country_id => country_10.id,
+                             :user_type_id => user_type_5.id, :gender => 'male',
+                             :title => "Mr")
+end
 
