eccc3da351607f4b2f42a83fc986d2d9551b865d
Martin <martin.me15@yahoo.com>
2016-04-14 10:28:38 +0700

    adding validation for payment confirmation

--- aea/app/models/payment_confirmation.rb
+++ aea/app/models/payment_confirmation.rb
@@ -1,3 +1,8 @@
 class PaymentConfirmation < ActiveRecord::Base
   belongs_to :user
+
+  validates :sender_name, :presence => true
+  validates :amount, :presence => true, :numericality => true
+  validates :payment_date, :presence => true
+
 end
