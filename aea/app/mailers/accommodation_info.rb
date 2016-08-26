4e8beffbba572dd4d0a8be9e3185fbb67707fa38
Martin <martin.me15@yahoo.com>
2016-08-26 11:35:23 +0700

    adding attchemnt

--- aea/app/mailers/accommodation_info.rb
+++ aea/app/mailers/accommodation_info.rb
@@ -5,7 +5,8 @@ class AccommodationInfo < ApplicationMailer
     @pu = user.pick_up_schedule
     @arriving_time = user.arriving_time
     @domain = domain
-    mail(to: "martin.me15@yahoo.com", subject: "[AEA GA 2016] ACCOMMODATION") do |format|
+    attachments['AEA_GA_2016.pdf'] = File.read('public/AEA_GA_2016.pdf')
+    mail(to: @user.email, subject: "[AEA GA 2016] ACCOMMODATION") do |format|
       format.html
     end
   end
@@ -15,6 +16,7 @@ class AccommodationInfo < ApplicationMailer
     @pu = user.pick_up_schedule
     @arriving_time = user.arriving_time
     @domain = domain
+    attachments['AEA_GA_2016.pdf'] = File.read('public/AEA_GA_2016.pdf')
     mail(to: ["aeaga2016@gmail.com", "martin.me15@yahoo.com"], subject: "[AEA GA 2016] ACCOMMODATION - Admin") do |format|
       format.html
     end
